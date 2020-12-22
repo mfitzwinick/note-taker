2a12bf2cf85a531c04d217b6eb2e329687851f1d
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-12-22 14:53:35 -0800

    fixing code, read me draft

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# note-taker
+
+# MICHELE'S NOTE TAKER -- READ.ME
+
+# Description/Purpose
+
+My note taker is a tool for people who need to create notes for themselves on the go. A user can press the "Get Started" Button and then input and save notes for themselves.  Once a task has been completed, it is easily deleted from the list.
+
+## Languages Used
+
+- HTML
+- CSS
+- JAVASCRIPT / JQUERY
+- NODE.JS
+
+## Libraries/3rd Party APIs 
+
+The site pulls from the following libraries and third-party APIs:
+- Express
+- JQuery
+- Bootstrap 
+- Font Awesome (https://use.fontawesome.com/releases/v5.3.1)
+- It requires fs (file system) and path module.
+
+
+## Features
+
+Michele's Note Taker begins with a home page centered on a "Get Started" Button.  This button (via a href) will redirect the user to the notes.html.  Once there, the user can input a title and text of a note and save it as a list item.  The note can me edited and deleted as needed.
+
+
+## File Structure
+
+The site is built with two html pages: index.html (home page) and notes.html (note input/edit/delete page).  
+
+The index.js file contains the ajax calls that outline the get, post and delete requests.  It also contains the save functionality allowing the saved notes to render when the page is opened.
+
+The db.json file is the database that outlines the keys of the object.  They are the title, text and id. 
+
+The server.js file outlines the routes, and establishes the PORT (process.env.PORT || 8080).
+
+Front end files are nested in the "public" folder, which include the html, css and index.js files.
+
+## Examples/Screenshots
+
+## Future Development
+
+Michele's Note Taker (2.0) will include a more modern and pleasing UI.  It will also allow notes to be edited, and will allow the user delete notes from the page without refreshing -- functionality coming!  It will also include a date function which will allow the user to track how long ago a note was posted.
+
+
+
--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-// Dependencies
-// =============================================================
+// Dependencies//
 var express = require("express");
 var path = require("path");
 const notes = require("./db/db.json");
 var fs = require("fs");
 
-// Sets up the Express App
-// =============================================================
+// Sets up the Express App//
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Middleware for Express app to handle data parsing
+// Sets up the Middleware for Express app to handle data parsing//
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + 'public'))
 
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
+// Routes//
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
@@ -27,7 +21,7 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-//IM CONFUSED ABOUT THIS?????/////
+
 app.get("/assets/css/styles.css", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"));
 });
@@ -35,20 +29,16 @@ app.get("/assets/css/styles.css", function (req, res) {
 app.get("/assets/js/index.js", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/assets/js/index.js"));
 });
-//////////
-app.get("/api/notes", function (req, res) {
 
+app.get("/api/notes", function (req, res) {
     return res.json(notes)
 
 });
 
 
-
-// Displays a single note, or returns false
 app.get("/api/notes/:note", function (req, res) {
     var noteInput = req.params.note;
-
-    console.log(noteInput);
+    // console.log(noteInput);//
 
     for (var i = 0; i < notes.length; i++) {
         if (noteInput === notes[i].title) {
@@ -59,34 +49,28 @@ app.get("/api/notes/:note", function (req, res) {
     return res.json(false);
 });
 
-
-
 app.post("/api/notes", function (req, res) {
-
     var newNote = req.body;
     newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
-    console.log(newNote);
+    //console.log(newNote);//
 
     notes.push(newNote);
-
     res.json(newNote);
 });
 
 app.delete('/api/notes/:id', function (req, res) {
     var deletedNote = req.params.id
-    console.log("we are here we are here we are here")
-    
-  for (var i = 0; i < notes.length; i++) {
-    if (deletedNote === notes[i].id) {
-        notes[i]={}
+
+    for (var i = 0; i < notes.length; i++) {
+        if (deletedNote === notes[i].id) {
+            notes[i] = {}
+        }
     }
-}
 })
 
 
+// Starts the server to begin listening on PORT//
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
